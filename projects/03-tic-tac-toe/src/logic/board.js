d3b24e85a5f970b0f631bd15285430a5662c3f5a
Hugo <hugo@boahstudio.com>
2023-02-19 01:08:14 -0300

    new game: tic tac toe

---
+++ projects/03-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,26 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si x o o gana
+    for(const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c] 
+      ) {
+        // si hay ganador
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+}
+
+export const checkEndGame = (newBoard) => {
+    // revisamos si hay empate
+    // si no hay mas espacios vacios
+    // en el tabler
+    return newBoard.every((square) => square !== null)
+}
